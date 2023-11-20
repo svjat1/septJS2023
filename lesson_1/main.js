a82efbd108fb426c18f6876d99cebb566cf251e1
svjat1 <calipsossss@gmail.com>
2023-11-20 22:30:03 +0200

    First HW JS sept 2023

---
+++ lesson_1/main.js
@@ -0,0 +1,44 @@
+let s = 'hello';
+console.log(s);
+let s1 = 'owu';
+console.log(s1);
+let s2 = 'com';
+console.log(s2);
+let s3 = 'ua';
+console.log(s3);
+let n1 = 1;
+console.log(n1);
+let n2 = 10;
+console.log(n2);
+let n3 = -999;
+console.log(n3);
+let n4 = 123;
+console.log(n4);
+let n5 = 3.14;
+console.log(n5);
+let n6 = 2.7;
+console.log(n6);
+let n7 = 16;
+console.log(n7);
+let n8 = true;
+console.log(n8);
+let n9 = false;
+console.log(n9);
+
+firstName = 'Sviatoslav';
+middleName = 'Petrovych';
+lastName = 'Kapets'
+person = firstName +' '+ middleName +' '+ lastName
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+name = prompt('Write your name')
+midlNamePrompt = prompt('Your middleName')
+year = prompt('Current year')
+console.log(name +' '+ midlNamePrompt +' '+ year);
