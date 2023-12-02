dacda98686332387189aeea76d74efda6ff3a54d
svjat1 <calipsossss@gmail.com>
2023-12-03 00:33:09 +0200

    4-th HW JS sept 2023

---
+++ lesson_4/main4.js
@@ -0,0 +1,114 @@
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+function calcSofCube(a, b) {
+    return a * b
+}
+
+let result_calcSofCube = calcSofCube(10, 5);
+console.log(result_calcSofCube);
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+function calcSofColo(r) {
+    return 2 * r * Math.PI;
+}
+
+let result_calcSofColo = calcSofColo(10);
+console.log(result_calcSofColo);
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+function calcSofCylinder(h, r) {
+    return 2 * r * h * Math.PI;
+}
+
+let result_calcSofCylinder = calcSofCylinder(1, 5);
+console.log(result_calcSofCylinder);
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+function obj(arr) {
+    for (const argument of arr) {
+        console.log(argument);
+    }
+    return arr;
+}
+
+obj([2, 22, ['test'], {key: "value"}]);
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+function paragraphWrite(text) {
+    document.write(`<p> ${text} </p>`)
+}
+
+paragraphWrite('Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+function createUl(text) {
+    document.write(`<div>
+                         <ul>
+                         <li>${text}</li>
+                         <li>${text}</li>
+                         <li>${text}</li>
+                         </ul>
+                    </div>`)
+                         }
+createUl('lorem lorem lorem two')
+createUl('Перевірочка перевірочка')
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+// Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+function createUlLi(text, number) {
+    document.write(`<ul>`)
+    for (let i = 0; i < number; i++) {
+        document.write(`<li>${text}</li>`)
+    }
+    document.write(`</ul>`)
+}
+createUlLi('Тестуємо власні логічні потуги', 3)
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+function createObj(arr) {
+    for (const argument of arr) {
+        document.write(`
+                         <ul>
+                         <li>${argument}</li>
+                         </ul>
+                         `)
+    }
+}
+createObj([33, 44, 'test','test2', false]);
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ.
+// // Для кожного об'єкту окремий блок.
+function createObjectWithKey(arr){
+    for (const arrElement of arr) {
+            document.write(`<div> ID : ${arrElement.id} Name: ${arrElement.name} Age: ${arrElement.age}</div>`)
+        }
+    }
+createObjectWithKey([
+    {id:44, name:'OLEH', age :14},
+    {id:14, name:'Mamai', age :33},
+    {id:11, name:'Korifey', age :25}
+]);
+// - створити функцію яка повертає найменьше число з масиву
+function smallestNumber (arr){
+    let min = arr[0]
+    for (const arrElement of arr) {
+        if(arrElement < min)
+             min = arrElement
+    }
+    return min;
+}
+
+console.log(smallestNumber([2, 11, 22, 82, 113, 6, 1]));
+// - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+// Приклад sum([1,2,10]) //->13
+function sumNumber (arr){
+    let result = 0
+    for(const argument of arr) {
+        result += argument
+    }
+    return result
+}
+
+let resw = sumNumber([2, 11, 22, 82, 113, 6, 1])
+console.log(resw);
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
